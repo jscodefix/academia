18bb1cadf905b1f56dcc2f3f22a6ed782234b5c3
Jeff Sheffel <jscodefix@sheffel.org>
2023-08-13 18:57:16 -0600

    add working version of JS arrayFlatten() with Jest test

--- javascript/array-flatten.mjs
+++ javascript/array-flatten.mjs
@@ -1,10 +1,26 @@
 /*
-
+This array flatten challenge was inspired by a React Denver Discord post (by Jeff B.?).
  */
 
 export function arrayFlatten(input) {
-  if ( input.type !== Array ) {
+  if ( !Array.isArray(input) ) {
     throw new TypeError();
   }
-  return [];
+
+  let flattened = [];
+  let inner_array = [];
+  let element = input.shift();
+
+  while ( element !== undefined ) {
+    if ( typeof(element) === 'number' ) {
+      flattened.push(element);
+    } else {
+      inner_array = arrayFlatten(element);
+      inner_array.forEach((element) => {
+        flattened.push(element)
+      });
+    }
+    element = input.shift();
+  }
+  return flattened;
 }
--- javascript/array-flatten.test.mjs
+++ javascript/array-flatten.test.mjs
@@ -5,52 +5,57 @@ test code taken from https://gist.github.com/bcawrse/04a17dae7f534de1da214a1c056
 MODIFICATIONS:
 230801 jscodefix original version
  */
-import { arrayFlatten } from './array-flatten.mjs';
-//const flatten = require('./array-flatten.mjs', null , null);
+import {arrayFlatten} from './array-flatten.mjs';
+//const flatten = require('./array-flatten.mjs', null , null);  // CommonJS modules
 
-describe('array-flatten.mjs', () => {
-  test('arrayFlatten() throws exception', () => {
-    expect(() => { arrayFlatten() }).toThrow(TypeError);
+describe('array-flatten()', () => {
+  describe('error conditions', () => {
+    test('with no argument throws TypeError exception', () => {
+      expect(() => arrayFlatten()).toThrow(TypeError);
+    });
+    test('with undefined throws TypeError exception', () => {
+      expect(() => arrayFlatten(undefined)).toThrow(TypeError);
+    });
+    test('with a number throws TypeError exception', () => {
+      expect(() => arrayFlatten(5)).toThrow(TypeError);
+    });
+    test('with a string throws TypeError exception', () => {
+      expect(() => arrayFlatten('z')).toThrow(TypeError);
+    });
+    test('with an object throws TypeError exception', () => {
+      expect(() => arrayFlatten({x: 1})).toThrow(TypeError);
+    });
+    test('with array containing an object throws TypeError exception', () => {
+      expect(() => arrayFlatten([1, {x: 2}, 3])).toThrow(TypeError);
+    });
   });
-  test('arrayFlatten(undefined) throws exception', () => {
-    expect(() => { arrayFlatten(undefined) }).toThrow(TypeError);
+
+  test('with empty array returns an empty array', () => {
+    expect(arrayFlatten([])).toEqual([]);
   });
-  test('arrayFlatten(5) throws exception', () => {
-    expect(() => { arrayFlatten(5) }).toThrow(TypeError);
+  test('returns same array that is already flattened', () => {
+    expect(arrayFlatten([1, -2, 3.33, .4e-3])).toEqual([1, -2, 3.33, .4e-3]);
   });
-  test('arrayFlatten(\'z\') throws exception', () => {
-    expect(() => { arrayFlatten('z') }).toThrow(TypeError);
+  test('flattens a simple nested array of arrays', () => {
+    expect(arrayFlatten([1, 2, [3, [4, 5]], 6])).toEqual([1, 2, 3, 4, 5, 6]);
   });
-  test('arrayFlatten([]) = []', () => {
-    expect(arrayFlatten([]))
-      .toEqual([]);
-  });
-  test('arrayFlatten([1, 2, [], 4]) = [1, 2, 4]', () => {
-    expect(arrayFlatten([1, 2, [], 4]))
-      .toEqual([1, 2, 4]);
-  });
-  test('arrayFlatten([1, 2, 3, 4]) = [1, 2, 3, 4]', () => {
-    expect(arrayFlatten([1, 2, 3, 4]))
-      .toEqual([1, 2, 3, 4]);
+  test('flattens empty array elements by removing them', () => {
+    expect(arrayFlatten([1, 2, [], 4])).toEqual([1, 2, 4]);
   });
   test('arrayFlatten([[1, 2, [3]], 4]) = [1, 2, 3, 4]', () => {
-    expect(arrayFlatten([[1, 2, [3]], 4]))
-      .toEqual([1, 2, 3, 4]);
+    expect(arrayFlatten([[1, 2, [3]], 4])).toEqual([1, 2, 3, 4]);
   });
   test('arrayFlatten([1, 2, [1, 2, 3]] = [1, 2, 1, 2, 3]', () => {
-    expect(arrayFlatten([1, 2, [1, 2, 3]]))
-      .toEqual([1, 2, 1, 2, 3]);
+    expect(arrayFlatten([1, 2, [1, 2, 3]])).toEqual([1, 2, 1, 2, 3]);
   });
-  test('arrayFlatten([1, 2, [1, 2, 3]] != [1, 1, 2, 2, 3]', () => {
-    expect(arrayFlatten([1, 2, [1, 2, 3]]))
-      .not.toEqual([1, 1, 2, 2, 3]);
+  test('does not sort integer elements', () => {
+    expect(arrayFlatten([1, 2, [1, 2, 3]])).not.toEqual([1, 1, 2, 2, 3]);
   });
-  test('arrayFlatten([1, 2, [3, 2, 1, [1, 2, 3, 4, 5], 1, 2, 3], 3, 4, 5]) = [1, 2, 3, 2, 1, 1, 2, 3, 4, 5, 1, 2, 3, 3, 4, 5]', () => {
+  test('handles deeply nested arrays', () => {
     expect(arrayFlatten([1, 2, [3, 2, 1, [1, 2, 3, 4, 5], 1, 2, 3], 3, 4, 5]))
       .toEqual([1, 2, 3, 2, 1, 1, 2, 3, 4, 5, 1, 2, 3, 3, 4, 5]);
   });
-  test('arrayFlatten([[[1], 2]]) = [1, 2]', () => {
-    expect(arrayFlatten([[[1], 2]]))
-      .toEqual([1, 2]);
+  test('handles identity nesting', () => {
+    expect(arrayFlatten([[[1], 2]])).toEqual([1, 2]);
   });
 });
